8344ef383b8115ee931246c2efa00f36708b67f2
Vladimir Dulenov <vladimir.dulenov@zattoo.com>
2022-12-02 16:05:09 +0100

    Simple config pkg added

---
+++ pkg/conf/config.go
@@ -0,0 +1,20 @@
+package conf
+
+type Config struct {
+	ServerHost              string   `yaml:"server_host"`
+	ServerPort              string   `yaml:"server_port"`
+	LogLevel                string   `yaml:"log_level"`
+	LogEncoding             string   `yaml:"log_encoding"`
+	LoggerColor             bool     `yaml:"logger_color"`
+	LoggerDisableStacktrace bool     `yaml:"logger_disable_stacktrace"`
+	LoggerDevMode           bool     `yaml:"logger_dev_mode"`
+	LoggerDisableCaller     bool     `yaml:"logger_disable_caller"`
+	LoggerDisabledHttp      []string `yaml:"log_disabled_http"`
+	VBB                     VBB      `yaml:"vbb"`
+}
+
+type VBB struct {
+	ScanSecInterval int    `yaml:"scan_interval"`
+	API             string `yaml:"api_endpoint"`
+	APIMaxResults   int    `yaml:"api_max_result"`
+}
---
+++ pkg/conf/defaults.go
@@ -0,0 +1,46 @@
+package conf
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+)
+
+const DefaultConfigPath = "/etc/ch_config.yaml"
+
+// Default returns config's object with default values
+func Default() *Config {
+	return &Config{
+		LogLevel:                "info",
+		LogEncoding:             "console",
+		LoggerColor:             true,
+		LoggerDisableStacktrace: true,
+		LoggerDevMode:           true,
+		LoggerDisableCaller:     false,
+		LoggerDisabledHttp:      []string{"/version"},
+		ServerHost:              "127.0.0.1",
+		ServerPort:              "8081",
+		VBB: VBB{
+			ScanSecInterval: 90,
+			API:             "https://v5.vbb.transport.rest",
+			APIMaxResults:   10,
+		},
+	}
+}
+
+// ReadConfigFile reading and parsing configuration yaml file
+func (conf *Config) ReadConfigFile(configPath string) {
+	if configPath == "" {
+		configPath = DefaultConfigPath
+	}
+	yamlConfig, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = yaml.Unmarshal(yamlConfig, &conf)
+	if err != nil {
+		fmt.Printf("Could not unmarshal config %v", conf)
+		log.Fatal(err)
+	}
+}
