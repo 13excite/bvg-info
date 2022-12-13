38163d68367a524ba8ee3a274bfa05327a67d346
Vladimir Dulenov <vladimir.dulenov@zattoo.com>
2022-12-13 14:20:46 +0100

    cmd/root.go is ready(?)

--- cmd/root.go
+++ cmd/root.go
@@ -1,51 +1,60 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/spf13/cobra"
+	_ "net/http/pprof" // Import for pprof
+
+	cli "github.com/spf13/cobra"
+	"go.uber.org/zap"
+
+	"github.com/13excite/bvg-info/pkg/conf"
 )
 
+var (
+	configFile string
+	pidFile    string
+	logger     *zap.SugaredLogger
 
-
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "bvg-info",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
-}
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
+	// The Root Cli Handler
+	rootCmd = &cli.Command{
+		Version: conf.GitVersion,
+		Use:     conf.Executable,
+		PersistentPreRunE: func(cmd *cli.Command, args []string) error {
+			// Create Pid File
+			pidFile = conf.C.PidFile
+			if pidFile != "" {
+				file, err := os.OpenFile(pidFile, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+				if err != nil {
+					return fmt.Errorf("Could not create pid file: %s Error:%v", pidFile, err)
+				}
+				defer file.Close()
+				_, err = fmt.Fprintf(file, "%d\n", os.Getpid())
+				if err != nil {
+					return fmt.Errorf("Could not create pid file: %s Error:%v", pidFile, err)
+				}
+			}
+			return nil
+		},
+		PersistentPostRun: func(cmd *cli.Command, args []string) {
+			// Remove Pid file
+			if pidFile != "" {
+				os.Remove(pidFile)
+			}
+		},
 	}
-}
+)
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bvg-info.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVarP(&configFile, "config", "c", "", "config file")
 }
 
+// Execute starts the program
+func Execute() {
+	// Run the program
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
 
+}
